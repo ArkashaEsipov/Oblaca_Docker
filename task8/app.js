d996786b5897212da46fd38e0c78b6421c1c9923
ArkashaEsipov <afark.vvve@yandex.ru>
2025-10-29 19:11:12 +0300

    fix task8

--- task8/app.js
+++ task8/app.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const { createClient } = require('redis');
+const redis = require('redis');
 
+const client = redis.createClient(6379, 'redis'); // подключение к Redis
 const app = express();
-const redis = createClient({ url: 'redis://redis:6379' });
 
-redis.connect().then(() => redis.set('visits', 0));
+// Установим visits = 0 при старте
+client.set('visits', 0);
 
-app.get('/', async (req, res) => {
-  const visits = await redis.incr('visits');
-  res.send(`<h1>Hello!</h1><p>Visited ${visits} times.</p>`);
+app.get('/', (req, res) => {
+  client.incr('visits', (err, visits) => {
+    if (err) return res.status(500).send(err.toString());
+    res.send(`<h1>Hello!</h1><p>Visited ${visits} times.</p>`);
+  });
 });
 
 app.listen(3002, () => console.log('Server running on port 3002'));
